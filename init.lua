412c8bf3301362f99aa62d9fd4089d37aa33adb0
Theo Fabien <73390220+TheoFABIEN@users.noreply.github.com>
2026-06-30 12:48:04 +0200

    Update Vim configuration in init.lua
    Set tab and indentation options, enable line numbers, and configure split behavior.

--- init.lua
+++ init.lua
@@ -1,4 +1,14 @@
-require "keymaps"
-require "options"
-require "plugins.lazy"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.swapfile = false
+
+vim.opt.splitright = true
+
+require("config.lazy")
